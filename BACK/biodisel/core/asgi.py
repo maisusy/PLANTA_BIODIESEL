a784ec5613756bb006b05f9fba0463789697dc90
maisusy <maisusy15@outlook.com>
2023-06-26 09:17:16 -0300

    Cambio nombre de carpeta back

--- BACK/biodisel/core/asgi.py
+++ BACK/biodisel/core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for escuela project.
+ASGI config for biodisel project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'escuela.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biodisel.settings')
 
 application = get_asgi_application()
