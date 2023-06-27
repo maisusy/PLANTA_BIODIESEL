3dd9309882c06d7013a62c89870d679decc8925c
maisusy <maisusy15@outlook.com>
2023-06-27 15:28:17 -0300

    INTENTO PUT Y DELETE

--- biodisel-back/Familia/urls.py
+++ biodisel-back/Familia/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from Familia.views import (
     familia_lista,
+    familia_id
 )
 
 urlpatterns = [
     path('api', familia_lista.as_view()),
+    path('api/<int:id>/', familia_id.as_view()),
 ]
