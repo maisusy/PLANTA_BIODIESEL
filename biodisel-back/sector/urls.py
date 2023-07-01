9bac5dce4ea0c8d8604f71693c69e2b4edf0d21c
susana martinez <maisusy15@outlook.com>
2023-07-01 13:31:02 -0300

    POST & DELETE sector

--- biodisel-back/sector/urls.py
+++ biodisel-back/sector/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from sector.views import (
     sector_lista,
+    sector_id,
 )
 
 urlpatterns = [
     path('api', sector_lista.as_view()),
+    path('api/<int:id>/', sector_id.as_view()),
 ]
