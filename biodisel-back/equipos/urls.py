7adef072a0928d6321b37f88abe322889684dba0
susana martinez <maisusy15@outlook.com>
2023-07-01 13:26:49 -0300

    POST & DELETE equipos

--- biodisel-back/equipos/urls.py
+++ biodisel-back/equipos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from equipos.views import (
     equipos_lista,
+    equipos_id
 )
 
 urlpatterns = [
     path('api', equipos_lista.as_view()),
+    path('api/<int:id>/', equipos_id.as_view()),
 ]
