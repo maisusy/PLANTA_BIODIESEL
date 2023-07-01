380e6a780df8dc627d80538132efe08c66ba28f7
susana martinez <maisusy15@outlook.com>
2023-07-01 13:29:08 -0300

    ṔOST & DELETE planta

--- biodisel-back/planta/urls.py
+++ biodisel-back/planta/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from planta.views import (
     planta_lista,
+    planta_id
 )
 
 urlpatterns = [
     path('api', planta_lista.as_view()),
+    path('api/<int:id>/', planta_id.as_view()),
 ]
--- biodisel-back/planta/views.py
+++ biodisel-back/planta/views.py
@@ -36,3 +36,69 @@ class planta_lista(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class planta_id(APIView):
+
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self, id):
+        '''
+        Helper method to get the object with given id, and user_id
+        '''
+        try:
+            return Planta.objects.get(id=id)
+        except Planta.DoesNotExist:
+            return None
+    # 2. Retrieve
+    def get(self, request, id, *args, **kwargs):
+        '''
+        Retrieves the Todo with given todo_id
+        '''
+        todo_instance = self.get_object(id)
+        if not todo_instance:
+            return Response(
+                {"res": "Object with todo id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = PlantaSerializer(todo_instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+     # 3. Update
+    def put(self, request, id, *args, **kwargs):
+        '''
+        Updates the todo item with given id if exists
+        '''
+        todo_instance = self.get_object(id)
+        if not todo_instance:
+            return Response(
+                {"res": "Object with todo id does not exists"}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        data = {
+            'codigo': request.data.get('codigo'), 
+            'nombre': request.data.get('nombre'), 
+        }
+        serializer = PlantaSerializer(instance = todo_instance, data=data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # 4. Delete
+    def delete(self, request, id, *args, **kwargs):
+        '''
+        Deletes the todo item with given id if exists
+        '''
+        todo_instance = self.get_object(id)
+        if not todo_instance:
+            return Response(
+                {"res": "Object with todo id does not exists"}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        todo_instance.delete()
+        return Response(
+            {"res": "Object deleted!"},
+            status=status.HTTP_200_OK
+        )
+    
