c9571c49f8e463b9648f3dfe00d393e465ba3b53
susana martinez <maisusy15@outlook.com>
2023-07-02 22:10:14 -0300

    Cambio logos navbar

--- biodisel-front/src/app/menunav/menunav.component.ts
+++ biodisel-front/src/app/menunav/menunav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { environment as env } from '../../environments/environments';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-menunav',
@@ -9,13 +11,15 @@ import { MenuItem } from 'primeng/api';
 export class MenunavComponent implements OnInit {
   items: MenuItem[] | undefined;
   activeItem: MenuItem | undefined;
+  public env = env;
+
+
+  constructor(private router: Router,
+     private route: ActivatedRoute){}
+
 
     ngOnInit() {
         this.items = [
-            {
-                label: 'Inventario',
-                icon: 'pi pi-fw pi-file',
-            },
             {
                 label: 'Inventario',
                 icon: 'pi pi-fw pi-file',
@@ -35,14 +39,37 @@ export class MenunavComponent implements OnInit {
             {
               label: 'Equipos',
               icon: 'pi pi-fw pi-calendar',
-          },
+            },
         ];
         this.activeItem = this.items[0];
     }
 
     onActiveItemChange(event: MenuItem) {
+
+
+
         this.activeItem = event;
+ 
+        switch(event.label){
+        case 'Equipos' : 
+            this.router.navigate(['equipos']);
+        break;
+        case 'Inventario' : 
+            this.router.navigate(['inventario']);
+        break;
+        case 'Familias' : 
+            this.router.navigate(['familia']);
+        break;
+        case 'Sectores' : 
+            this.router.navigate(['sector']);
+        break;
+        case 'Planta' : 
+            this.router.navigate(['planta']);
+        break;
     }
+
+        console.log('aaaa', event.label )
+     }
 
 
 }
