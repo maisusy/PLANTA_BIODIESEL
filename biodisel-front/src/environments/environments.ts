1ca8bbd05337dd6bef08fc5210927d3e2fb4c68c
susana martinez <maisusy15@outlook.com>
2023-07-03 14:31:34 -0300

    fix imagenes

--- biodisel-front/src/environments/environments.ts
+++ biodisel-front/src/environments/environments.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    apiURL: "http://127.0.0.1:8000"
+    apiURL: "http://127.0.0.1:8000",
+    myVarForImageURLS: 'http://localhost:4200/assets/'
   };
   
